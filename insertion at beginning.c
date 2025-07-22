e8e70ab231bb0665ee7c3cdd122ffce48ac2ab3b
ThivyaVenkatachalam <thivyavenkat15@gmail.com>
2025-07-22 18:17:53 +0530

    Create insertion at beginning.c

---
+++ insertion at beginning.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include<stdlib.h>
+
+ struct node {
+     int d;
+     struct node *next;
+ }; 
+ 
+  struct node * create()
+  {
+      struct node *head;
+      head=(struct node*)malloc(sizeof(struct node));
+      head->next=NULL;
+      return head;
+  }
+  
+  
+  void insert(struct node *head, int e)
+  {
+    struct node *newnode=(struct node*)malloc(sizeof(struct node));
+    newnode->d=e;
+    newnode->next=head->next;
+    head->next=newnode;
+  }
+  
+  
+  void display(struct node *head)
+  {
+      struct node * temp=head->next;
+      while(temp!=NULL)
+      {
+          if(temp->d%5==0)
+          {
+              printf("%d \n" , temp->d);
+          }
+          temp=temp->next;
+      }
+  }
+  
+  int main()
+  {
+      struct node *head= create();
+      for(int i=1;i<=100;i++)
+      {
+          insert(head,i);
+      }
+      display(head);
+      return 0;
+  }
+  
