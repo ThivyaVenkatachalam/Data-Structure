bdd2bf92591f57b5591df194f443e4f48f25b2a2
ThivyaVenkatachalam <thivyavenkat15@gmail.com>
2025-07-28 22:50:27 +0530

    Create delete using element.c

---
+++ delete using element.c
@@ -0,0 +1,94 @@
+#include <stdio.h>
+#include<stdlib.h>
+
+struct node{
+    int d;
+    struct node* next;
+};
+
+struct node* create()
+{
+    struct node* head;
+    head=(struct node*)malloc(sizeof(struct node));
+    head->next=NULL;
+    return head;
+}
+void insert(struct node*head,int pos,int n)
+{
+    struct node*prev=head;
+    int i=0;
+    while(i<(pos-1)&&prev!=NULL)
+    {
+        prev=prev->next;
+        i++;
+    }
+    if(prev==NULL)
+    {
+       printf("exceeds limit");
+    }
+    else
+    {
+        struct node *newnode = (struct node*)malloc(sizeof(struct node));
+        newnode->d=n;
+        newnode->next=prev->next;
+        prev->next=newnode;
+    }
+}
+
+
+void display(struct node* head)
+{
+    struct node*temp=head->next;
+    while(temp!=NULL)
+    {
+        printf("%d\n",temp->d);
+        temp=temp->next;
+    }
+}
+void delete(struct node* head,int n)
+{
+    
+    if(head->next==NULL)
+    {
+        printf("list is empty");
+    }
+    else{
+        struct node*prev=head;
+        int i=0;
+        while(prev->next!=NULL&&prev->next->d!=n)
+        {
+            prev=prev->next;
+            i++;
+        }
+        if(prev->next==NULL)
+        {
+            printf("NOT FOUND");
+            
+           
+        }
+        else
+        {
+            struct node* temp=prev->next;
+            prev->next=prev->next->next;
+            free(temp);
+            printf("deleted\n");
+        }
+        
+    }
+}
+
+int main()
+{
+
+    struct node* head=create();
+    insert(head,1,1);
+    insert(head,2,2);
+    insert(head,3,3);
+    insert(head,4,4);
+    insert(head,5,5);
+    display(head);
+    delete(head,3);
+    display(head);
+    
+    return 0;
+}
